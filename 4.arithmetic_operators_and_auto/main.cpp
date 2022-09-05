9abe512ab5d2821effb10d0bc3b75cd8d1cfcc94
charcoalSkies <jun48470@gmail.com>
2022-09-05 22:29:19 +0900

    기본 연산자

---
+++ 4.arithmetic_operators_and_auto/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    float a = 10.0;
+    int b = 3;
+    int c = 5;
+
+    cout << a / b * c << endl;
+
+
+/*-----------------------------------------------*/
+
+    //auto
+    auto n = 100;       // int 
+    auto x = 1.5;       // float
+
+    
+    return 0;
+}
