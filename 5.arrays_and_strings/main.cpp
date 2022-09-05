952a9d06979996c74a9b5b5477d44ecb139929fa
charcoalSkies <jun48470@gmail.com>
2022-09-05 22:29:45 +0900

    배열

---
+++ 5.arrays_and_strings/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short month[12] = {1, 2, 3};
+
+    cout << month[3] << endl;
+
+
+
+    char a[5] = {'H', 'e', 'l', 'l', 'o'};
+
+    char b[] = "Hello";
+
+    cout << a <<endl;
+    cout << b << endl;
+    return 0;
+}
